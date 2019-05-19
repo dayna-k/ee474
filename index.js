862f82b6735437df99943992c8c31a9076f21897
dayna-k <dayeon4552@gmail.com>
2019-05-19 20:15:11 +0900

    merge into app.js

--- index.js
+++
@@ -1,4 +0,0 @@
-// index.js
-function btn1() {
-  console.log("btn1 clicked");
-}
---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+// index.js
+function btn1() {
+  console.log("btn1 clicked");
+}
+
+$("#btn1").click(function () {
+  console.log("btn1 clicked in routes index.js");
+});
