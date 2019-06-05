e7ae4fa678afc725e154c59f790eaffee2f70844
dayna-k <dayeon4552@gmail.com>
2019-06-06 01:54:39 +0900

    almost done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,16 +13,17 @@
 
 var input_img = ""
 var color_list = [["var(--color1)", "var(--color2)", "var(--color3)", "var(--color4)", "var(--color5)", "var(--color6)", "var(--color7)", "var(--color8)", "var(--color9)", "var(--color10)"],
-["var(--default_s_2)", "var(--color2)", "var(--color3)", "var(--color4)", "var(--color5)", "var(--color6)", "var(--color7)", "var(--color8)", "var(--color9)", "var(--color10)"],
-["var(--default_s_3)", "var(--color2)", "var(--color3)", "var(--color4)", "var(--color5)", "var(--color6)", "var(--color7)", "var(--color8)", "var(--color9)", "var(--color10)"],
-["var(--default_s_4)", "var(--color2)", "var(--color3)", "var(--color4)", "var(--color5)", "var(--color6)", "var(--color7)", "var(--color8)", "var(--color9)", "var(--color10)"],
-["var(--default_s_5)", "var(--color2)", "var(--color3)", "var(--color4)", "var(--color5)", "var(--color6)", "var(--color7)", "var(--color8)", "var(--color9)", "var(--color10)"]];
+["var(--default_s_2)", "var(--color2_s_2)", "var(--color3_s_2)", "var(--color4_s_2)", "var(--color5_s_2)", "var(--color6_s_2)", "var(--color7_s_2)", "var(--color8_s_2)", "var(--color9_s_2)", "var(--color10_s_2)"],
+["var(--default_s_3)", "var(--color2_s_3)", "var(--color3_s_3)", "var(--color4_s_3)", "var(--color5_s_3)", "var(--color6_s_3)", "var(--color7_s_3)", "var(--color8_s_3)", "var(--color9_s_3)", "var(--color10_s_3)"],
+["var(--default_s_4)", "var(--color2_s_4)", "var(--color3_s_4)", "var(--color4_s_4)", "var(--color5_s_4)", "var(--color6_s_4)", "var(--color7_s_4)", "var(--color8_s_4)", "var(--color9_s_4)", "var(--color10_s_4)"],
+["var(--default_s_5)", "var(--color2_s_5)", "var(--color3_s_5)", "var(--color4_s_5)", "var(--color5_s_5)", "var(--color6_s_5)", "var(--color7_s_5)", "var(--color8_s_5)", "var(--color9_s_5)", "var(--color10_s_5)"]];
 
 var prev_t = 0;
-var prev_t_color_r = 0;
-var prev_t_color_c = 0;
+var prev_t_color_r = 3;
+var prev_t_color_c = 1;
 var prev_b = 0;
-var prev_b_color = 0;
+var prev_b_color_r = 3;
+var prev_b_color_c = 1;
 var top_count = 0;
 
 
@@ -73,15 +74,12 @@ $("#color_table_top td").click(function(){
 $("#color_table_top_s td").click(function(){
   var table_top_s = document.getElementById("color_table_top_s");
   var table_bottom_s = document.getElementById("color_table_bottom_s");
+  var topval = document.getElementById("topval");
   var ts_td = $(this);
   var tr = $(this).parent();
   var row = 0;
   var col = 0;
   top_count = 0;
-
-  // console.log("clicked td: ", ts_td);
-  // console.log("tr: ", tr);
-  console.log("클릭된 top color는 "+ ts_td.text());
 
   if (ts_td.text()%10 == 0){
     row = parseInt(ts_td.text()/10)-1;
@@ -90,16 +88,20 @@ $("#color_table_top_s td").click(function(){
     row = parseInt(ts_td.text()/10);
     col = ts_td.text()%10;
   }
-  for (var j=0; j<5; j++){
-        table_top_s.rows[j].cells[col-1].classList.remove("color_h_clicked");
-  }
-
-  this.classList.add("color_h_clicked");
   prev_t_color_r = row;
   prev_t_color_c = col;
-  console.log("row: ", prev_t_color_r);
-  console.log("col: ", prev_t_color_c);
-  top_count = 1;
+
+  if (prev_t == prev_t_color_c){
+    console.log("클릭된 top color는 "+ ts_td.text());
+    console.log("row: ", prev_t_color_r);
+    console.log("col: ", prev_t_color_c);
+    console.log("prev: ", prev_t);
+    for (var j=0; j<5; j++){
+          table_top_s.rows[j].cells[col-1].classList.remove("color_h_clicked");
+    }
+    this.classList.add("color_h_clicked");
+    topval.innerHTML='&nbsp&nbsp<span style="background-color: '+color_list[row-1][col-1]+'; word-spacing:10px">&nbsp&nbsp;</span>'
+  }
 
 })
 
@@ -113,8 +115,8 @@ $("#color_table_bottom td").click(function(){
   var table_bottom_s = document.getElementById("color_table_bottom_s");
 
   console.log("클릭된 top color는 "+td.text());
-  prev_t = td.text();
-  console.log(prev_t);
+  prev_b = td.text();
+  console.log(prev_b);
 
   /* border setting, color setting of s_table */
   for (var i=0; i<10; i++){
@@ -147,32 +149,70 @@ $("#color_table_bottom td").click(function(){
   }
 })
 
-/*
-var slider1 = document.getElementById("myRange1");
-var slider1_val = document.getElementById("slidevalue1");
+$("#color_table_bottom_s td").click(function(){
+  var table_top_s = document.getElementById("color_table_top_s");
+  var table_bottom_s = document.getElementById("color_table_bottom_s");
+  var topval = document.getElementById("bottomval");
+  var ts_td = $(this);
+  var tr = $(this).parent();
+  var row = 0;
+  var col = 0;
+  top_count = 0;
 
-var slider2 = document.getElementById("myRange2");
-var slider2_val = document.getElementById("slidevalue2");
+  if (ts_td.text()%10 == 0){
+    row = parseInt(ts_td.text()/10)-1;
+    col = 10;
+  }else{
+    row = parseInt(ts_td.text()/10);
+    col = ts_td.text()%10;
+  }
+  prev_b_color_r = row;
+  prev_b_color_c = col;
 
-// Update the current slider value (each time you drag the slider handle)
-slider1.oninput = function() {
-  slider1_val.innerHTML = ": "+this.value;
-  localStorage.setItem('top_val', slider1.value);
-}
+  if (prev_b == prev_b_color_c){
+    console.log("클릭된 bottom color는 "+ ts_td.text());
+    console.log("row: ", prev_b_color_r);
+    console.log("col: ", prev_b_color_c);
+    console.log("prev: ", prev_b);
+    for (var j=0; j<5; j++){
+          table_bottom_s.rows[j].cells[col-1].classList.remove("color_h_clicked");
+    }
+    this.classList.add("color_h_clicked");
+    bottomval.innerHTML='&nbsp&nbsp<span style="background-color: '+color_list[row-1][col-1]+'; word-spacing:10px">&nbsp&nbsp;</span>'
+  }
+})
 
-// Update the current slider value (each time you drag the slider handle)
-slider2.oninput = function() {
-  slider2_val.innerHTML = ": "+this.value;
-  localStorage.setItem('bottom_val', slider2.value);
-}*/
+
 
 function btn_color(){
+  var color_top_s = document.getElementById("color_box_top_s");
+  var color_bottom_s = document.getElementById("color_box_bottom_s");
+
   console.log("btn_color clicked");
-  console.log("top_val: ", localStorage.getItem('top_val'))
-  console.log("bottom_val: ", localStorage.getItem('bottom_val'))
-  //$("#output_img")[0].src = "/images/output/"+input_img;
+  // console.log("top_val: ", localStorage.getItem('top_val'))
+  // console.log("bottom_val: ", localStorage.getItem('bottom_val'))
+  console.log("top_color_h: ", prev_t_color_c);
+  console.log("top_color_s: ", prev_t_color_r);
+  console.log("bottom_color_h: ", prev_b_color_c);
+  console.log("bottom_color_s: ", prev_b_color_r);
+  if(color_top_s.style.display != 'none'){
+    // color_top_s.style.display = 'block';
+    $("#color_box_top_s").slideUp(500);
+  }
+  if(color_bottom_s.style.display != 'none'){
+    // color_top_s.style.display = 'block';
+    $("#color_box_bottom_s").slideUp(500);
+  }
   $("#output_img")[0].src = "/py_code/output/output.jpg";
-  $("#cmd_submit").click();
+  //$("#output_img")[0].src = "/images/output/"+input_img;
+  setTimeout(function() {
+    $("#output_img")[0].src = "/py_code/output/output.jpg";
+    $("#output_img").fadeIn(500);
+  }, 1500);
+
+  // $("#output_img")[0].src = "/py_code/output/997_1.jpg";
+  // $("#output_img").slideDown(500, function(){});
+  // $("#cmd_submit").click();
 }
 
 function btn_reset(){
@@ -204,25 +244,27 @@ $(document).ready(function () {
   console.log(localStorage.getItem('output_img'))
   // console.log(localStorage.getItem('top_val'))
   // console.log(localStorage.getItem('bottom_val'))
-  $("#input_img")[0].src = localStorage.getItem('input_img');
-  $("#output_img")[0].src = localStorage.getItem('output_img');
+  // $("#input_img")[0].src = localStorage.getItem('input_img');
+  // $("#output_img")[0].src = localStorage.getItem('output_img');
   // $("#myRange1")[0].value = localStorage.getItem('top_val');
   // slider1_val.innerHTML = ": "+$("#myRange1")[0].value;
   // $("#myRange2")[0].value = localStorage.getItem('bottom_val');
   // slider2_val.innerHTML = ": "+$("#myRange2")[0].value;
+  /*
   setTimeout(function() {
     console.log('Reload!')
     console.log(localStorage.getItem('input_img'))
     console.log(localStorage.getItem('output_img'))
     // console.log(localStorage.getItem('top_val'))
     // console.log(localStorage.getItem('bottom_val'))
-    $("#input_img")[0].src = localStorage.getItem('input_img');
-    $("#output_img")[0].src = localStorage.getItem('output_img');
+    // $("#input_img")[0].src = localStorage.getItem('input_img');
+    // $("#output_img")[0].src = localStorage.getItem('output_img');
     // $("#myRange1")[0].value = localStorage.getItem('top_val');
     // slider1_val.innerHTML = ": "+$("#myRange1")[0].value;
     // $("#myRange2")[0].value = localStorage.getItem('bottom_val');
     // slider2_val.innerHTML = ": "+$("#myRange2")[0].value;
   }, 4000);
+  */
 
 
   $("#getimg").change(function () {
@@ -253,7 +295,24 @@ $(document).ready(function () {
 
 });
 
+/*
+var slider1 = document.getElementById("myRange1");
+var slider1_val = document.getElementById("slidevalue1");
 
+var slider2 = document.getElementById("myRange2");
+var slider2_val = document.getElementById("slidevalue2");
+
+// Update the current slider value (each time you drag the slider handle)
+slider1.oninput = function() {
+  slider1_val.innerHTML = ": "+this.value;
+  localStorage.setItem('top_val', slider1.value);
+}
+
+// Update the current slider value (each time you drag the slider handle)
+slider2.oninput = function() {
+  slider2_val.innerHTML = ": "+this.value;
+  localStorage.setItem('bottom_val', slider2.value);
+}*/
 
 //
 // function exec_cmd(input){
